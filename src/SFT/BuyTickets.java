1a8642b499a3e92ac8999a5648b1562c206d4a48
Lin Tang <1508014225@qq.com>
2022-07-30 20:09:42 +0800

    完成所有算法题

--- src/SFT/BuyTickets.java
+++ src/SFT/BuyTickets.java
@@ -2,28 +2,27 @@ package SFT;
 
 public class BuyTickets {
     public static void main(String[] args) {
-        int[] a= new int[]{2,3,2};
-        System.out.println(timeRequiredToBuy(a,2));
+        int[] a = new int[]{2, 3, 2};
+        System.out.println(timeRequiredToBuy(a, 2));
     }
 
     public static int timeRequiredToBuy(int[] tickets, int k) {
-        int n=tickets.length;
-        int result=0;
-        while(true){
-        for (int i = 0; i <n ; i++) {
-            if(i==k&&tickets[i]==1){
-                tickets[i]-=1;
-                result++;
+        int n = tickets.length;
+        int result = 0;
+        while (true) {
+            for (int i = 0; i < n; i++) {
+                if (i == k && tickets[i] == 1) {
+                    tickets[i] -= 1;
+                    result++;
+                    break;
+                } else if (tickets[i] != 0) {
+                    tickets[i] -= 1;
+                    result++;
+                }
+            }
+            if (tickets[k] == 0) {
                 break;
             }
-            else if (tickets[i]!=0){
-                tickets[i]-=1;
-                result++;
-            }
-        }
-        if(tickets[k]==0){
-            break;
-        }
         }
         return result;
     }
